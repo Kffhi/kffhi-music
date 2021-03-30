34015452cecf49310f525a5d5eb8be7b963eb5a6
wuhao <13979215061@163.com>
2021-03-30 18:59:26 +0800

    chore: router初始化

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,10 @@ export default defineConfig({
     nodeModulesTransform: {
         type: 'none'
     },
-    routes: [{ path: '/', component: '@/pages/index' }],
+    routes: [
+        { path: '/', component: '@/pages/home/index' },
+        { path: '/page', component: '@/pages/page/index' }
+    ],
     fastRefresh: {},
     extraBabelPlugins: [
         ['import', { libraryName: 'antd-mobile', style: true }] //按需加载 antd-mobile 样式文件
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState, FC } from 'react'
+import { Button } from 'antd-mobile'
+import classNames from 'classnames'
+
+import styles from './index.less'
+
+const pageIndex: FC = () => {
+    const [text, setText] = useState('初555')
+
+    const clickText = () => {
+        setText('得得得')
+    }
+
+    return (
+        <div className={classNames(styles.title)} onClick={clickText}>
+            测试
+            <Button>{text}</Button>
+        </div>
+    )
+}
+
+export default pageIndex
